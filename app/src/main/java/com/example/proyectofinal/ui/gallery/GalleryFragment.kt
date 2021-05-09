7701fb22ed3ca78f5866f6ecec75544db18b1a5d
abigail <mariana.aby@gmail.com>
2021-05-08 20:08:04 -0500

    sección de outfit base completa

--- app/src/main/java/com/example/proyectofinal/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/proyectofinal/ui/gallery/GalleryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -13,19 +14,24 @@ import com.example.proyectofinal.R
 class GalleryFragment : Fragment() {
 
     private lateinit var galleryViewModel: GalleryViewModel
+    private lateinit var imageViewTorso: ImageView
+    private lateinit var imageViewPiernas: ImageView
+    private lateinit var imageViewPies: ImageView
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-                ViewModelProvider(this).get(GalleryViewModel::class.java)
+        galleryViewModel = ViewModelProvider(this).get(GalleryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        imageViewTorso=root.findViewById(R.id.image_outfit_torso)
+        imageViewPiernas=root.findViewById(R.id.image_outfit_piernas)
+        imageViewPies=root.findViewById(R.id.image_outfit_pies)
+        imageViewTorso.setImageResource(R.drawable.ropados)
+        imageViewPiernas.setImageResource(R.drawable.ropados)
+        imageViewPies.setImageResource(R.drawable.ropados)
+
         return root
     }
 }
